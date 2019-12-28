fb8b9b203a11300f2edea98ebb8576bc28649664
adir <adiro@wix.com>
2019-12-28 18:39:57 +0200

    update firebase playground storage and collections

--- firebaseTest.js
+++ firebaseTest.js
@@ -2,12 +2,21 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAcountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "text-summarization-262015.appspot.com"
 });
 
 const db = admin.firestore();
-
+const bucket = admin.storage().bucket();
 const run = async () => {
-    const collection = await db.collection('images').get()
+  const collection = await db.collection('images').get()
+  await bucket.upload('./test1.jpg', {
+    gzip: true,
+    metadata: {
+      cacheControl: 'public, max-age=31536000',
+    },
+  });
+  await bucket.file('test1.jpg').download({destination: './test123.jpg', validation: false});
+
 }
 run();
