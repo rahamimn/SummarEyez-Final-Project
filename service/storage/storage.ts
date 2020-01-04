b705b6c8dfd5420440510609e3019299e4424154
adir <adiro@wix.com>
2020-01-04 18:28:28 +0200

    update structure

---
+++ service/storage/storage.ts
@@ -0,0 +1,11 @@
+import googleStorage from './googleStorage/googleStorage';
+import localStorage from './localStorage/localStorage';
+import storageMock from './mock/storage.mock';
+export interface Storage{
+
+}
+
+
+const storages: Storage = [googleStorage, localStorage, storageMock ];
+
+
