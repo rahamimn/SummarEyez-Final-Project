ed28e2b30b760352ba9657aedb2ca93716f19fe8
adir <adiro@wix.com>
2020-03-13 15:48:53 +0200

    when add image runs automatic algs

--- server/src/services/experimentService.ts
+++ server/src/services/experimentService.ts
@@ -1,7 +1,9 @@
 import { fileTypes, Storage } from "./storage/storageTypes";
 import { Collections } from "./collections/collectionsTypes";
 import { PythonScriptInterface } from "./pythonScripts/pythonScriptsTypes";
-
+const forEP = require('foreach-promise');
+//@ts-ignore
+import {promises as fs} from 'fs';
 
 export class ExperimentService{
     private collectionsService : Collections;
@@ -23,6 +25,27 @@ export class ExperimentService{
         });
     };
 
+    private addAutomaticAlgToImg = async (tables, imageName) => {
+        await forEP(tables, async row => {
+            const path = `images/${imageName}/algs/${row.name}`;
+            await this.storageService.uploadBuffer(path, row.sent_table, fileTypes.Csv);
+            await this.collectionsService.images().sentTablesOf(imageName).add(row.name,{
+                type: 'automatic',
+                name: row.name,
+                path
+            });
+        });
+    }
+    private getAutoAlgsSavedLocally = async () => {
+        const files = await fs.readdir('./automatic-algorithms');
+        let algNames = [];
+        await forEP(files, async (file:string) => {
+            if(file.endsWith('.py')){
+                algNames.push(file);
+            }
+        });
+        return algNames;
+    }
     addImage = async (name, buffer) => {
         const files = await this.pythonService.processImage(buffer)
         await this.storageService.uploadBuffer(`images/${name}/image`, buffer, fileTypes.Image);
@@ -36,8 +59,17 @@ export class ExperimentService{
             text_path: `images/${name}/text`,
             word_ocr_path: `images/${name}/word_ocr`,
             base_sent_table_path: `images/${name}/base_sent_table`,
-        })
-        //should run automatic-algs
+        });
+
+        //we should fetch newly algorithms 
+
+        const automaticAlgsSavedLocally = await this.getAutoAlgsSavedLocally()
+       
+        const {tables} = await this.pythonService.runAutomaticAlgs(
+            automaticAlgsSavedLocally,
+            files.text,
+            files.base_sent_table);
+        await this.addAutomaticAlgToImg(tables,name);
     }
 
     getImages = async () => {
@@ -46,6 +78,7 @@ export class ExperimentService{
 
         return images;
     }
+
     //TODO - check if exists download if needed
     // we could check with in memory data
     private verifyAutomaticAlgorithmExists = async (names: string[]) => {};
@@ -55,16 +88,7 @@ export class ExperimentService{
         const base_sent_table = options.base_sent_table || await this.storageService.downloadToBuffer(`images/${imageName}/base_sent_table`);
         await this.verifyAutomaticAlgorithmExists(algsNames);
         const {tables} = await this.pythonService.runAutomaticAlgs(algsNames, text,base_sent_table);
-        //should replace in foreach-await 
-        tables.forEach(async row => {
-            const path = `images/${imageName}/algs/${row.name}`;
-            await this.storageService.uploadBuffer(path, row.sent_table, fileTypes.Csv);
-            await this.collectionsService.images().sentTablesOf(imageName).add(row.name,{
-                type: 'automatic',
-                name: row.name,
-                path
-            })
-        })
+        await this.addAutomaticAlgToImg(tables,imageName);
     }
     
 
