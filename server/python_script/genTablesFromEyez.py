0d89efe995c2bd955b61a9733d7cf997dae9973c
etay <etay21@gmail.com>
2020-03-27 16:50:01 +0300

    adding normal implement for word and sentence table

--- server/python_script/genTablesFromEyez.py
+++ server/python_script/genTablesFromEyez.py
@@ -1,25 +1,41 @@
 #get gaze locations and generate new word/sentece grade table.
 import pandas as pd 
 import numpy as np
+from runAutomaticAlgo import normalWeight
 
 def find_index_of_nearest_xy(y_array, x_array, y_point, x_point):
     distance = (y_array - y_point) ** 2 + (x_array - x_point) ** 2
     idx = np.where(distance == distance.min())
     return int(idx[0])
 
+
 def normalize_weights(word_table, sent_table):
-    max_sent_value = sent_table['weight'].max()
-    min_sent_value = sent_table['weight'].min()
-
-    max_word_value = word_table['weight'].max()
-    min_word_value = word_table['weight'].min()
-
-    #normal
-    word_table = word_table.copy()
-    sent_table['weight'] = (sent_table['weight'] - min_sent_value) / (max_sent_value - min_sent_value)
-    word_table['weight'] = (word_table['weight'] - min_word_value) / (max_word_value - min_word_value)
-
+    normalize_table(sent_table, True)
+    normalize_table(word_table, False)
     return word_table, sent_table
+        
+#to_devide_len define if(like in sent_table) we should devide by number of charecter or not
+def normalize_table(table, to_devide_len):
+    table['normalized_weight'] = table['weight'].astype(float)
+    max_sent_value1 = table['normalized_weight'].max()
+    min_not_zero = max_sent_value1 
+    for num in table['normalized_weight']:
+        if num < min_not_zero and num != 0:
+            min_not_zero= num
+
+     # minus 20%       
+    min_not_zero= 0.8 * min_not_zero
+
+    for i in range(len(table['normalized_weight'])):
+        if table['normalized_weight'][i] == 0:
+            table['normalized_weight'][i] = min_not_zero
+
+    if to_devide_len is True:
+        for i in range(len(table['normalized_weight'])):
+            table['normalized_weight'][i] = (min_not_zero / table['char_count'][i])
+    
+    normalWeight(table)
+
 
 def calculate_weight(fixations, word_table, sentences_table):
     x_array = np.array(word_table['center_x'])
@@ -39,7 +55,7 @@ def calculate_weight(fixations, word_table, sentences_table):
 
     word_table, sentences_table = normalize_weights(word_table, sentences_table)
     
-    word_table = word_table[['text', 'sent_num', 'par_num', 'weight']]
+    word_table = word_table[['text', 'sent_num', 'par_num', 'weight', 'normalized_weight']]
     return word_table, sentences_table
 
 def main(fixations, word_ocr, base_sentences_table):
