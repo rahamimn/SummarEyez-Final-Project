e89a93ca37ea751e01dfe82b8bcd1a3533e13a66
rahamimn <rahamimn@post.bgu.ac.il>
2020-04-11 15:52:28 +0300

    merge ui button conditional enabled

--- client/src/components/Experiments/Summaries/MergeSummaries/MergeDialog.js
+++ client/src/components/Experiments/Summaries/MergeSummaries/MergeDialog.js
@@ -8,7 +8,9 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import api from '../../../../apiService';
 import TableMerge from './TableMerge';
 import {useParams} from 'react-router-dom'
-import CircularProgress from '@material-ui/core/CircularProgress'
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 
 
@@ -22,25 +24,32 @@ export default function MergeDialog({
   ]
 
   const {experimentName} = useParams();
-
-  var percentTotalSum = () => 
-    mergeInput.reduce((totPercent, record) => totPercent + record.percentage,0);
   
   const [mergeInput, setMergeInput] = useState(addToInput(selected))
   const [mergeName, setMergeName] = useState("")
   const [isLoading, setIsLoading] = useState(false)
+  const [textColor, setTextColor] = useState("inherit")
+  const [isSubmitted, setIsSubmitted] = useState(false)
 
 
   const setPercentageOf = (index) => (value) => {
     const mergeInputCopy = [...mergeInput];
     mergeInputCopy[index].percentage = value
-    setMergeInput(mergeInputCopy)
+    setMergeInput(mergeInputCopy);
+    (percentTotalSum() === 1) ? setTextColor("primary") : setTextColor("error");
   }
 
-  const isDisabled = percentTotalSum() !== 1;
+  const handleChangeName = (event) =>
+    setMergeName(event.target.value)
 
-  const handleChangeName = (event) => setMergeName(event.target.value)
+  const percentTotalSum = () =>
+    mergeInput.reduce((totPercent, record) => totPercent+record.percentage, 0)
+
+  const isWeightSumValid = () => 
+    percentTotalSum() === 1;
   
+  const isDisabled = !isWeightSumValid() || mergeName.length === 0 || isSubmitted;
+
 
     return (
     <div>
@@ -57,34 +66,43 @@ export default function MergeDialog({
           <DialogContentText>
           <TableMerge mergeInput={mergeInput} setPercentageOf={setPercentageOf}/>
           </DialogContentText>
-        
-          
+          <div>
+          <MuiThemeProvider theme={theme}>
+            <Typography color={textColor} variant="subtitle1">
+              Total: {parseInt((percentTotalSum()*100), 10)}
+            </Typography>
+          </MuiThemeProvider>
+          </div>
+
+          <div>
           <TextField 
                 // error={this.state.isNameExists}
                 // helperText={this.state.isNameExists && "Name already exsits, please choose different name" }
                 value={mergeName}
-                style={{marginBottom: '20px'}}
+                style={{marginBottom: '10px'}}
                 onChange={handleChangeName}
                 id="standard-basic"
-                label="Insert merge-product name"
+                label="Type new name..."
                 />
-
+          </div>
 
           <Button
+            color="primary"
             disabled={isDisabled}
             onClick={
               async () => {
+                setIsSubmitted(true)
                 setIsLoading(true);
                 const res = await api.mergeAlgorithms(experimentName, mergeName, mergeInput);
                 setIsLoading(false);
                 onClose(mergeName);
               }
               }>Create</Button>
-              
-          
+
+
           {isLoading &&
             <div style={{ width: '100%', textAlign:'center', marginTop:'20px'}}>
-              <CircularProgress />
+              <LinearProgress/>
             </div>
           }
 
@@ -93,3 +111,5 @@ export default function MergeDialog({
     </div>
   );
 }
+
+  const theme = createMuiTheme({ palette: { primary: { main: "#004f29" } } } );
