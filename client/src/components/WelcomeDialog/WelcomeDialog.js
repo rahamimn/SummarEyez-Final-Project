63d6ed5f1fbc01edfcb34a91f84d50529f99a963
rahamimn <rahamimn@post.bgu.ac.il>
2020-05-06 20:53:34 +0300

    WelcomeDialog Beautify

--- client/src/components/WelcomeDialog/WelcomeDialog.js
+++ client/src/components/WelcomeDialog/WelcomeDialog.js
@@ -1,28 +1,20 @@
 import React,{useState, useEffect} from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import {
-  useHistory,
-  useParams
-} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 import api from '../../apiService';
+import TabPanel from './TabPanel';
 
 export default function WelcomeDialog({
   onClose,
   permit,
-  experimentName
 }) {
   const history = useHistory();
   const [permKey,setPermKey] = useState();
   const [experimentText,setExperimentText] = useState();
-  const [experiment,setExperiment] = useState();
   const [experiments,setExperiments] = useState();
-  const [isKeyError,setKeyError] = useState(false);
   
   useEffect(() => {
     fetchExperiments();
@@ -41,7 +33,6 @@ export default function WelcomeDialog({
     return false;
   }
 
-
   return (
     <div>
       <Dialog 
@@ -51,97 +42,12 @@ export default function WelcomeDialog({
         disableBackdropClick = {!!!onClose}
         disableEscapeKeyDown = {!!!onClose}
         >
-        <DialogTitle id="form-dialog-title">Choose Mode</DialogTitle>
+        <DialogTitle id="form-dialog-title">Welcome to SummarEYEZ </DialogTitle>
         <DialogContent>
           <DialogContentText>
-            To continue please choose mode. 
+            Please choose your option:
           </DialogContentText>
-          <TextField
-            error={isKeyError}
-            helperText="Incorrect Key"
-            autoFocus
-            margin="dense"
-            id="welcome-dialog-permission-input" 
-            label="permission key"
-            value={permKey}
-            onChange={(e) => setPermKey(e.target.value)}
-            type="password"
-            fullWidth
-          />
-          <Autocomplete
-            id="experiment-select"
-            style={{ width: '100%', marginRight:10 }}
-            options={experiments}
-            autoHighlight
-            getOptionLabel={option => option.id}
-            renderInput={params => (
-              <TextField
-                {...params}
-                label="Choose an experiment"
-                fullWidth
-                inputProps={{
-                    ...params.inputProps,
-                    autoComplete: 'disabled', // disable autocomplete and autofill
-                }}
-              />
-            )}
-            onChange={(e,experiment) => 
-              setExperiment(experiment)
-            }
-            onInputChange={(e, value) => 
-              setExperimentText(value)
-            }
-            inputValue={experimentText}
-
-          />
-          <div style={{
-              display: 'flex',
-              justifyContent: 'flex-end',
-              margin: '30px 0 10px'
-          }}>
-            <Button
-                    id="welcome-dialog-new-experiment"
-                    variant="contained"
-                    size="large"
-                    style={{marginRight: '10px'}}
-                    onClick={() => {
-                      onClose && onClose();
-                      validate() ? history.push(experimentName ? 
-                        `/experiments/${experimentName}/new` :
-                        `/experiments-new`
-                      ) : setKeyError(true);
-                    }}
-                    >
-                New Experiments
-            </Button>
-            <Button
-                    disabled={!experiment}
-                    variant="contained"
-                    size="large"
-                    style={{marginRight: '10px'}}
-                    onClick={() => {
-                      onClose && onClose();
-                      validate() ? 
-                        history.push(`/experiments/${experiment.id}`) :
-                        setKeyError(true);
-                    }}
-                    >
-                Go To Experiment
-            </Button>
-            <Button
-                    // disabled={true}
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                    onClick={() => {
-                      onClose && onClose();
-                      validate();
-                      history.push('/tests');
-                    }}
-                    >
-                Tests
-            </Button>
-            </div>
+          <TabPanel permit={permit}></TabPanel>
         </DialogContent>
       </Dialog>
     </div>
