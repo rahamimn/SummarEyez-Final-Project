cdd3af98e7b452fd7e6aa3d7e1d148c669349e87
rahamimn <rahamimn@post.bgu.ac.il>
2020-05-10 21:31:35 +0300

    e2e tests-setup is useful now

--- server/src/utils/dataCreationForE2e.ts
+++ server/src/utils/dataCreationForE2e.ts
@@ -4,58 +4,131 @@ import { promises as fsPromises } from 'fs';
 
 export const dataCreation = async (experimetService: ExperimentService) => {
 
-    // EXP1
-    console.log('Starting: Create Exp1');
+    // expA
+    console.log('expA: creating...');
     const img = await fsPromises.readFile('./test1.jpg');
     const alg = await fsPromises.readFile('./automatic-algorithms-locally/Alg1.py');
     await experimetService.addAutomaticAlgorithms('algo1.py', alg);
     await experimetService.addImage('img1', img);
-    await experimetService.addExperiment('exp1', 'img1');
-    console.log('Done: Create Exp1');
+    await experimetService.addExperiment('expA', 'img1');
+    console.log('expA: Done!');
 
-    // EXP2 (Optional, takes time to create)
-    console.log('Starting: Create Exp2');
+    // expB (Optional, takes time to create)
+    console.log('expB: creating...');
     const img2 = await fsPromises.readFile('./test2.jpg');
     const alg2 = await fsPromises.readFile('./automatic-algorithms-locally/Alg2.py');
     await experimetService.addAutomaticAlgorithms('algo2.py', alg2);
     await experimetService.addImage('img2', img2);
-    await experimetService.addExperiment('exp2', 'img2');
-    console.log('Done: Create Exp2');
+    await experimetService.addExperiment('expB', 'img2');
+    console.log('expB: Done!');
 
-    // EXP3 (Optional, takes time to create)
-    console.log('Starting: Create Exp3');
+    // expC (Optional, takes time to create)
+    console.log('expC: creating...');
     const img3 = await fsPromises.readFile('./test1.jpg');
     const alg3 = await fsPromises.readFile('./automatic-algorithms-locally/Alg2.py');
     await experimetService.addAutomaticAlgorithms('algo3.py', alg3);
     await experimetService.addImage('img3', img3);
-    await experimetService.addExperiment('exp3', 'img3');
-    console.log('Done: Create Exp3');
+    await experimetService.addExperiment('expC', 'img3');
+    console.log('expC: Done!');
 
     //QUESTIONS:
-    const exp1_Q1 = await experimetService.addQuestion(
-        "exp1",
-        "Q1?",
-        ["a1", "a2", "a3", "a4"],
+    const QA1 = await experimetService.addQuestion(
+        "expA",
+        "Question number A1?",
+        ["Ans number 0", "Ans number 1", "Ans number 2", "Ans number 3"],
         "0"
     );
 
 
-    const exp1_Q2 = await experimetService.addQuestion(
-        "exp1",
-        "Q2?",
-        ["b1", "b2", "b3", "b4"],
+    const QA2 = await experimetService.addQuestion(
+        "expA",
+        "Question number A2?",
+        ["Ans number 0", "Ans number 1", "Ans number 2", "Ans number 3"],
         "1"
     );
 
-    // FORM1 - ALL OPTIONS
-    const form1 = await experimetService.addForm({
-        experimentName: 'exp1',
-        name: 'form1_all-optionz',
-        questionIds: [exp1_Q1.data.id, exp1_Q2.data.id],
+    const QB1 = await experimetService.addQuestion(
+        "expB",
+        "Question number B1?",
+        ["Ans number 0", "Ans number 1", "Ans number 2", "Ans number 3"],
+        "2"
+    );
+
+    const QC1 = await experimetService.addQuestion(
+        "expC",
+        "Question number C1?",
+        ["Ans number 0", "Ans number 1", "Ans number 2", "Ans number 3"],
+        "3"
+    );
+
+    // FORM A1 - ALL OPTIONS
+    const formA1 = await experimetService.addForm({
+        experimentName: 'expA',
+        name: 'formA1',
+        questionIds: [QA1.data.id, QA2.data.id],
         summary: { "filters": { "isGradient": true, "color": { "size": "8", "palete": "op_1" }, "minWeight": "0.1" } },
         isRankSentences: true,
         isReadSummary: true,
         isFillAnswers: true,
         withFixations: true
     });
+
+    // FORM A2 - ONLY QUESTIONS
+    const formA2 = await experimetService.addForm({
+        experimentName: 'expA',
+        name: 'formA2',
+        questionIds: [QA1.data.id, QA2.data.id],
+        summary: { "filters": { "isGradient": true, "color": { "size": "8", "palete": "op_1" }, "minWeight": "0.1" } },
+        isRankSentences: true,
+        isReadSummary: true,
+        isFillAnswers: true,
+        withFixations: true
+    });
+
+    // FORM B1 - ALL OPTIONS
+    const formB1 = await experimetService.addForm({
+        experimentName: 'expB',
+        name: 'formB1',
+        questionIds: [QB1.data.id],
+        summary: { "filters": { "isGradient": true, "color": { "size": "8", "palete": "op_1" }, "minWeight": "0.1" } },
+        isRankSentences: true,
+        isReadSummary: true,
+        isFillAnswers: true,
+        withFixations: true
+    });
+
+    // FORM C1 - ALL OPTIONS
+    const formC1 = await experimetService.addForm({
+        experimentName: 'expC',
+        name: 'formC1',
+        questionIds: [QC1.data.id],
+        summary: { "filters": { "isGradient": true, "color": { "size": "8", "palete": "op_1" }, "minWeight": "0.1" } },
+        isRankSentences: true,
+        isReadSummary: true,
+        isFillAnswers: true,
+        withFixations: true
+    });
+
+    // PLAN1 - Single Form
+    const plan1 = await experimetService.addTestPlan(
+        'Test Plan1',
+        [{ "experimentName": "expA", "formId": "formA1" }]
+        // Ask Adir about syntax here - How to use the fields names properly, I couldn't use them here.
+    )
+
+    // PLAN2 - TWO forms of TWO different experiments
+    const plan2 = await experimetService.addTestPlan(
+        'Test Plan2',
+        [{ "experimentName": "expA", "formId": "formA1" },
+        { "experimentName": "expB", "formId": "formB1" }]
+    )
+
+    // PLAN3 - THREE forms of THREE different experiments
+    const plan3 = await experimetService.addTestPlan(
+        'Test Plan3',
+        [{ "experimentName": "expA", "formId": "formA1" },
+        { "experimentName": "expB", "formId": "formB1" },
+        { "experimentName": "expC", "formId": "formC1" }]
+    )
+
 }
