ecaa16597bc1f110b3f7cf9b35a77ceea11cc65d
adir <adiro@wix.com>
2020-05-21 12:28:08 +0300

    fix bugon

--- client/src/components/Experiments/TestPool/TestHeaders.js
+++ client/src/components/Experiments/TestPool/TestHeaders.js
@@ -36,12 +36,15 @@ export const createHeadersFromForm = (form, experimentName, openQuestionModal, i
       })), 
       { id: 'score', label: 'Score' },
     ];
-    if(form.withFixations){
-      headers.push({ id: 'name', label: 'fixations-summary', type: 'custom', format: (name) => <Button onClick={() => window.open(`/article/${experimentName}/eyes/${name}`,'_blank')}>view</Button> })
-    }
-    if(form.isRankSentences){
-      headers.push({ id: 'sentanceWeights', label: 'rank-sentences', type: 'custom', format: (sentanceWeights) => <Button onClick={() => saveAs(new Blob([JSON.stringify(sentanceWeights)]), 'weights.json')}> download </Button> });
-    }
   }
+  
+  if(form.withFixations){
+    headers.push({ id: 'name', label: 'fixations-summary', type: 'custom', format: (name) => <Button onClick={() => window.open(`/article/${experimentName}/eyes/${name}`,'_blank')}>view</Button> })
+  }
+
+  if(form.isRankSentences){
+    headers.push({ id: 'sentanceWeights', label: 'rank-sentences', type: 'custom', format: (sentanceWeights) => <Button onClick={() => saveAs(new Blob([JSON.stringify(sentanceWeights)]), 'weights.json')}> download </Button> });
+  }
+  
   return headers;
 }
