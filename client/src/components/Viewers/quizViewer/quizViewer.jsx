89daf60a0b5271cc64a086ceccdcf8c633f62637
adir <adiro@wix.com>
2020-05-27 17:40:05 +0300

    added loader to quizViewer

--- client/src/components/Viewers/quizViewer/quizViewer.jsx
+++ client/src/components/Viewers/quizViewer/quizViewer.jsx
@@ -1,13 +1,15 @@
 import React ,{useState,useEffect} from 'react';
 import { BaseViewer } from "../BaseViewer/BaseViewer";
 import api from '../../../apiService';
+import { CircularProgress } from '@material-ui/core';
 
 export const QuizViewer = ({
   experimentName,
   type,
   name,
-  filters = {}
+  filters = {},
 }) => {
+  const [loader,setLoader] = useState(false);
     const [summaryDetails,setSummaryDetails] = useState({
       title: '',
       summary:[]
@@ -15,20 +17,24 @@ export const QuizViewer = ({
 
     useEffect(() => {
       const fetch = async () => {
+        setLoader(true);
         const res = await api.getSummary(experimentName,type,name);
         setSummaryDetails(res.data);
+        setLoader(false);
       }
       fetch();
     },[name, type, filters, experimentName]);
 
-    return <BaseViewer 
-            summary={summaryDetails.summary} 
-            title={summaryDetails.title}
-            filters ={{
-              color: filters.color || {size:'3', palete:'op_1'},
-              isGradient: filters.isGradient !== undefined ? filters.isGradient : true,
-              minWeight: filters.minWeight || 0,
-              topSentencesCount:21
-            }}/>
+    return loader ? <div style={{width:'100%', height:'100px', display:'flex',justifyContent:'center', alignItems:'center'}} ><CircularProgress size="60px"/></div> : 
+           <BaseViewer 
+              summary={summaryDetails.summary} 
+              title={summaryDetails.title}
+              filters ={{
+                color: filters.color || {size:'3', palete:'op_1'},
+                isGradient: filters.isGradient !== undefined ? filters.isGradient : true,
+                minWeight: filters.minWeight || 0,
+                topSentencesCount:21
+              }}/>
+          
 
 }
