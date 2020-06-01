a9f64e42da9bae8809f90f999a408462d22d20ab
etay93 <etay.lederman@vayyar.com>
2020-06-01 12:58:38 +0300

    update csv as meirav want

--- server/src/services/experimentService.ts
+++ server/src/services/experimentService.ts
@@ -260,12 +260,12 @@ testOfTestPlan = async (testPlanId, csv) =>{
                     }}
                 ];
                 if(test.data.answers && test.data.answers.length > 0){
-                    fields.push({label: 'score' , value: (entry) => testData(entry,index).score})
+                    fields.push({label: `[${test.data.formId}]-score`, value: (entry) => testData(entry,index).score})
                     test.data.answers.forEach((ans, i) => fields = [
                         ...fields,
-                        {label: `q${i} id` , value: (entry) => testData(entry,index).answers[i].id},
-                        {label: `q${i} answer` , value: (entry) => testData(entry,index).answers[i].ans},
-                        {label: `q${i} time` , value: (entry) => testData(entry,index).answers[i].time} 
+                        {label: `[${test.data.formId}]-q${i}` , value: (entry) => testData(entry,index).answers[i].id},
+                        {label: `[${test.data.formId}]-q${i} answer` , value: (entry) => testData(entry,index).answers[i].ans},
+                        {label: `[${test.data.formId}]-q${i} time` , value: (entry) => testData(entry,index).answers[i].time} 
                     ]);
                 }
             });
